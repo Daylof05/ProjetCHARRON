4505a59685b85c36f03ee7ab44c10f900645d07c
Daylof <90395763+Daylof05@users.noreply.github.com>
2024-02-22 16:30:20 +0100

    Updated

--- components/Activites.tsx
+++ components/Activites.tsx
@@ -30,76 +30,76 @@ const Activites: React.FC = () => {
 
     return (
         <View style={styles.container}>
-        <Text style={styles.activityText}>
-            {activite ? activite.activity : "Chargement de l'activité..."}
-        </Text>
-        {translatedActivity && <Text style={styles.translationText}>Traduction: {translatedActivity}</Text>}
-        <TextInput
-            style={styles.textInput}
-            placeholder="Nombre de participants"
-            keyboardType="numeric"
-            value={participants}
-            onChangeText={(text) => setParticipants(text.replace(/[^0-9]/g, ''))}
-        />
-        <TextInput
-            style={styles.textInput}
-            placeholder="Type d'activité"
-            value={type}
-            onChangeText={setType}
-        />
-        <Button
-            title='Nouvelle activité'
-            onPress={fetchData}
-        />
-        <View style={{ height: 10 }} />
-        {activite && (
-            <Button
-                title='Traduire'
-                onPress={handleTranslate}
+            <Text style={styles.activityText}>
+                {activite ? activite.activity : "Chargement de l'activité..."}
+            </Text>
+            {translatedActivity && <Text style={styles.translationText}>Traduction: {translatedActivity}</Text>}
+            <TextInput
+                style={styles.textInput}
+                placeholder="Nombre de participants"
+                keyboardType="numeric"
+                value={participants}
+                onChangeText={(text) => setParticipants(text.replace(/[^0-9]/g, ''))}
             />
-        )}
-    </View>
+            <TextInput
+                style={styles.textInput}
+                placeholder="Type d'activité"
+                value={type}
+                onChangeText={setType}
+            />
+            <Button
+                title='Nouvelle activité'
+                onPress={fetchData}
+            />
+            <View style={{ height: 10 }} />
+            {activite && (
+                <Button
+                    title='Traduire'
+                    onPress={handleTranslate}
+                />
+            )}
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      padding: 20,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f5f5f5', // Un fond légèrement gris pour réduire la fatigue oculaire
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5', 
     },
     textInput: {
-      height: 40,
-      width: '100%',
-      borderColor: '#cccccc',
-      borderWidth: 1,
-      borderRadius: 5,
-      marginTop: 10,
-      marginBottom: 10,
-      paddingLeft: 10,
-      backgroundColor: 'white', // Fond blanc pour les champs de texte, pour les faire ressortir
+        height: 40,
+        width: '100%',
+        borderColor: '#cccccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginTop: 10,
+        marginBottom: 10,
+        paddingLeft: 10,
+        backgroundColor: 'white', 
     },
     button: {
-      marginTop: 20,
-      width: '60%', // Réduire la largeur des boutons pour un aspect plus centré et aligné
-      borderRadius: 5,
+        marginTop: 20,
+        width: '60%', 
+        borderRadius: 5,
     },
     buttonText: {
-      textAlign: 'center',
+        textAlign: 'center',
     },
     activityText: {
-      fontSize: 18,
-      textAlign: 'center',
-      margin: 10,
+        fontSize: 18,
+        textAlign: 'center',
+        margin: 10,
     },
     translationText: {
-      fontSize: 16,
-      textAlign: 'center',
-      marginTop: 20,
-      color: 'gray', // Couleur grise pour distinguer la traduction
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 20,
+        color: 'gray',
     },
-  });
+});
 
 export default Activites;
